e3375ab91b252ec4ef486891b625ad6e894ec014
ankita-shankar <43548579+ankita-shankar@users.noreply.github.com>
2021-10-31 18:29:55 -0400

    adding some functions to model files (#3)
    * adding some functions to model files
    
    Co-authored-by: Ankita Shankar <ankita@swiftwind-mbp.local>

--- app/models/food_cart.rb
+++ app/models/food_cart.rb
@@ -1,4 +1,10 @@
 class FoodCart < ActiveRecord::Base
     belongs_to :user
     has_many :reviews, dependent: :destroy
+    @allowed_payment_options = ["cash", "card", "venmo"]
+
+    def self.get_all_reviews(food_cart_id)
+        food_cart = FoodCart.find(food_cart_id)
+        food_cart.reviews
+    end
 end
