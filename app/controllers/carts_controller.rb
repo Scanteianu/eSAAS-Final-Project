ec5a5aea158f454b99a30e60eba0b2c654cdcc16
Daniel Scanteianu <danielms853@gmail.com>
2021-11-02 09:23:26 -0400

    list all the carts on index, display more cart info on cart page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,18 +1,20 @@
 class CartsController < ApplicationController
 
   def index
-    #renders index Template
-    getCartFromDb(1)
+    @carts = FoodCart.all
   end
 
+
+  def cart
+    getCartFromDb(params[:id])
+  end
   def getCartFromDb(index)
     cartFromDb = FoodCart.find_by_id(index)
-    byebug
     cartToDisplay = Hash.new
 
     cartToDisplay[:name] = cartFromDb.name
     cartToDisplay[:location] = cartFromDb.location
-    cartToDisplay[:owner] = User.find_by_id(cartFromDb.user_id)
+    cartToDisplay[:owner] = User.find_by_id(cartFromDb.user_id).name
     cartToDisplay[:paymentOptions] = listifyPaymentOptions(cartFromDb.payment_options)
     cartToDisplay[:topRatedFood]= cartFromDb.top_rated_food
     cartToDisplay[:hours]= [cartFromDb.opening_time,cartFromDb.closing_time]
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'carts#index'
   get '/carts/:id' => 'carts#index'
+  get '/carts/cart/:id' => 'carts#cart'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
