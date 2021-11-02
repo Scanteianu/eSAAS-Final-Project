7dcdde584bcb957b9ee8233762f5b13337a8710a
Daniel Scanteianu <danielms853@gmail.com>
2021-11-02 19:51:40 -0400

    try to be explicit with primary key

--- db/migrate/20211029043204_create_users.rb
+++ db/migrate/20211029043204_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email_id,  primary_key: true
-      t.string :name
+      t.string :name, primary_key: false
 
-      t.timestamps null: false
+      t.timestamps null: false, primary_key: false
     end
   end
 end
