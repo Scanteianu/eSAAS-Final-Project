202c98f4e68e6b469a5138e43912ef668027b796
Daniel Scanteianu <danielms853@gmail.com>
2021-11-02 20:15:37 -0400

    move primary key to unique key

--- db/migrate/20211029043204_create_users.rb
+++ db/migrate/20211029043204_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email_id,  primary_key: true
+      t.string :email_id,  unique_key: true
       t.string :name
 
       t.timestamps null: false
