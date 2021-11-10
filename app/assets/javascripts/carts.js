26e8482d27886829d8ec806fafd4d693c5ba3fe3
Wesley Wei <wesley631w@gmail.com>
2021-11-10 00:24:20 -0500

    Added default Columbia location on map render and added sample markers for food carts

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -1,7 +1,45 @@
-var map;
+let map;
+const columbiaUniversityLatLng = {
+    lat: 40.80756390195308,
+    lng: -73.96260488871613
+};
+
+const foodCartCoords = [
+    {
+        name: 'Uncle Luoyang',
+        position: { 
+            lat: 40.80877070458994,
+            lng: -73.96310377081672
+        }
+    },
+    {
+        name: 'Chicken Guys',
+        position: {
+            lat: 40.80680499795128,
+            lng: -73.96078581662722
+        }
+    },
+    {
+        name: 'Shwarma Cart',
+        position: {
+            lat: 40.80753715418158,
+            lng: -73.96469123200576
+        }
+    }
+]
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: columbiaUniversityLatLng,
+        zoom: 15
+    });
+
+    foodCartCoords.forEach(foodCartCoord => {
+        console.log(foodCartCoord)
+        new google.maps.Marker({
+            position: foodCartCoord.position,
+            map,
+            title: foodCartCoord.name
+        });
     });
 }
